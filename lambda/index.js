146418102418024e3ab3997e35bd5ab820c472ae
Liam Nickell <liam.nickell25@gmail.com>
2021-01-18 01:07:47 -0600

    updated launch intent handler and registered handlers

--- lambda/index.js
+++ lambda/index.js
@@ -1,19 +1,29 @@
 const Alexa = require('ask-sdk-core');
 const LaunchRequestHandler = require('./intents/launch.js');
-const HelpIntentHandler = require('./intents/help.js');
-const ExitIntentHandler = require('./intents/exit.js');
+const NewSessionHandler = require('./intents/new-session.js');
 const SessionEndedRequestHandler = require('./intents/session-ended.js');
+const ExitIntentHandler = require('./intents/exit.js');
+const HelpIntentHandler = require('./intents/help.js');
+const RepeatIntentHandler = require('./intents/repeat.js');
+const FallbackHandler = require('./intents/fallback.js');
 const IntentReflectorHandler = require('./intents/reflector.js');
+const SaveResponseInterceptor = require('./interceptors/save-response.js');
 const ErrorHandler = require('./intents/error.js');
 
 
 exports.handler = Alexa.SkillBuilders.custom()
   .addRequestHandlers(
     LaunchRequestHandler,
-    HelpIntentHandler,
-    CancelAndStopIntentHandler,
+    NewSessionHandler,
     SessionEndedRequestHandler,
+    ExitIntentHandler,
+    HelpIntentHandler,
+    RepeatIntentHandler,
+    FallbackHandler,
     IntentReflectorHandler,
+  )
+  .addResponseInterceptors(
+    SaveResponseInterceptor,
   )
   .addErrorHandlers(
     ErrorHandler,
--- lambda/intents/launch.js
+++ lambda/intents/launch.js
@@ -7,7 +7,7 @@ const LaunchRequestHandler = {
   handle(handlerInput) {
     const speakOutput = 
       `Welcome to Song Match. I can help you understand which song by your favorite 
-      artist best matches your life. Please tell me the name of your favorite artist.`;
+      artist best matches your life. Who's your favorite artist?`;
     return handlerInput.responseBuilder
       .speak(speakOutput)
       .reprompt(speakOutput)
