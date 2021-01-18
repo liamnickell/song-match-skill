474cd8a2e07bfb39e882bcb5bd4b5926179ff999
Liam Nickell <liam.nickell25@gmail.com>
2021-01-17 22:39:13 -0600

    added new session handler to force user to open skill

---
+++ lambda/intents/new-session.js
@@ -0,0 +1,19 @@
+const Alexa = require('ask-sdk-core');
+
+// Handles new sessions that aren't launch requests by telling user to open skill
+const NewSessionHandler = {
+  canHandle(handlerInput) {
+    return Alexa.isNewSession(handlerInput.requestEnvelope);
+  },
+  handle(handlerInput) {
+    const speakOutput = 
+      `Sorry, I can't help with that. Say "open Song Match" if you want to play.`;
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      .withShouldEndSession(true)
+      .getResponse();
+  }
+};
+
+module.exports = NewSessionHandler;
