e4d075ea1881896bc968a57aadbfed3e41ec9d48
Liam Nickell <liam.nickell25@gmail.com>
2021-01-21 19:51:13 -0600

    added songs user can match with

---
+++ lambda/constants/songs.js
@@ -0,0 +1,48 @@
+const Songs = {
+  'runTheJewels': {
+    '000': `Angel Duster`,
+    '001': `Sea Legs`,
+    '010': `Talk to Me`,
+    '011': `Legend Has It`,
+    '100': `Ooh La La`,
+    '101': `Pulling the Pin`,
+    '110': `Oh My Darling Don't Cry`,
+    '111': `Blockbuster Night, Pt. 1`
+  },
+  'kendrickLamar': [
+    '000': `Good Kid`,
+    '001': `M.A.A.D City`,
+    '010': `Swimming Pools (Drank)`,
+    '011': `King Kunta`,
+    '100': `These Walls`,
+    '101': `How Much a Dollar Cost`,
+    '110': `The Blacker the Berry`,
+    '111': `Mortal Man`,
+    '200': `DNA`,
+    '201': `Element`,
+    '210': `Humble`,
+    '211': `Fear`
+  ],
+  'tameImpala': [
+    '000': `Alter Ego`,
+    '001': `Mind Mischief`,
+    '010': `Feels Like We Only Go Backwards`,
+    '011': `Elephant`,
+    '100': `Let It Happen`,
+    '101': `The Less I Know the Better`,
+    '110': `Borderline`,
+    '111': `Posthumous Forgiveness`
+  ],
+  'daftPunk': [
+    '000': `Around the World`,
+    '001': `One More Time`,
+    '010': `Harder, Better, Faster, Stronger`,
+    '011': `Robot Rock`,
+    '100': `Lose Yourself to Dance`,
+    '101': `Giorgio by Moroder`,
+    '110': `Get Lucky`,
+    '111': `Doin' It Right`
+  ]
+};
+
+module.exports = Songs;
