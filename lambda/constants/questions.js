af33044b751f6d9e618a7cb5b0304d974dbd5a3d
Liam Nickell <liam.nickell25@gmail.com>
2021-01-21 18:44:40 -0600

    added questions for song match quizzes

---
+++ lambda/constants/questions.js
@@ -0,0 +1,216 @@
+const Questions = {
+  'runTheJewels': [
+    {
+      'question': `Which scenery do you prefer: mountains or coasts?`,
+      'answers': [
+        {
+          'samples': [
+            'mountain'
+          ]
+        },
+        {
+          'samples': [
+            'coast'
+          ]
+        }
+      ]
+    },
+    {
+      'question': `You're speeding down the highway at 100 miles per hour when you suddenly spot a cop. Do you slam the brakes or floor it?`,
+      'answers': [
+        {
+          'samples': [
+            'brake',
+            'stop',
+            'slow'
+          ]
+        },
+        {
+          'samples': [
+            'floor',
+            'accelerate',
+            'speed'
+          ]
+        }
+      ]
+    },
+    {
+      'question': `If you were forced to run a fraudulent business, would you rather run a ponzi scheme or pyramid scheme?`,
+      'answers': [
+        {
+          'samples': [
+            'ponzi'
+          ]
+        },
+        {
+          'samples': [
+            'pyramid'
+          ]
+        }
+      ]
+    }
+  ],
+  'kendrickLamar': [
+    {
+      'question': `If you had to choose, would you rather have chocolate, vanilla, or coffee ice cream?`,
+      'answers': [
+        {
+          'samples': [
+            'chocolate'
+          ]
+        },
+        {
+          'samples': [
+            'vanilla'
+          ]
+        },
+        {
+          'samples': [
+            'coffee'
+          ]
+        }
+      ]
+    },
+    {
+      'question': `Yes or no: do you think we live in a simulation?`,
+      'answers': [
+        {
+          'samples': [
+            'yes'
+          ]
+        },
+        {
+          'samples': [
+            'no'
+          ]
+        }
+      ]
+    },
+    {
+      'question': `There's a bowl of freshly baked cookies in front of you and you're starving, but it's almost time for dinner. Do you eat a cookie or wait till after dinner?`,
+      'answers': [
+        {
+          'samples': [
+            'eat',
+            'now'
+          ]
+        },
+        {
+          'samples': [
+            'wait',
+            'after'
+          ]
+        }
+      ]
+    }
+  ],
+  'tameImpala': [
+    {
+      'question': `Yes or no: do you consider yourself a night owl?`,
+      'answers': [
+        {
+          'samples': [
+            'yes'
+          ]
+        },
+        {
+          'samples': [
+            'no'
+          ]
+        }
+      ]
+    },
+    {
+      'question': `Would you rather have the power to control electricity or water?`,
+      'answers': [
+        {
+          'samples': [
+            'electricity'
+          ]
+        },
+        {
+          'samples': [
+            'water'
+          ]
+        }
+      ]
+    },
+    {
+      'question': `You're in an important business meeting and very hungry. Would you rather eat an extra large pizza or saucepan full of spaghetti in front of everyone?`,
+      'answers': [
+        {
+          'samples': [
+            'pizza',
+            'extra large'
+          ]
+        },
+        {
+          'samples': [
+            'spaghetti',
+            'pasta',
+            'saucepan'
+          ]
+        }
+      ]
+    }
+  ],
+  'daftPunk': [
+    {
+      'question': `Assuming price isn't a factor, do you prefer Mac, Windows, or Linux?`,
+      'answers': [
+        {
+          'samples': [
+            'mac',
+            'apple'
+          ]
+        },
+        {
+          'samples': [
+            'windows',
+            'pc'
+          ]
+        },
+        {
+          'samples': [
+            'linux'
+          ]
+        }
+      ]
+    },
+    {
+      'question': `Assuming you aren't in dire need of hydration, would you rather drink still or sparkling water?`,
+      'answers': [
+        {
+          'samples': [
+            'still'
+          ]
+        },
+        {
+          'samples': [
+            'sparkling'
+          ]
+        }
+      ]
+    },
+    {
+      'question': `A waiter tells you to enjoy your meal and you accidentally reply with "you too." Do you acknowledge your mistake or avoid eye contact?`,
+      'answers': [
+        {
+          'samples': [
+            'acknowledge',
+            'mistake'
+          ]
+        },
+        {
+          'samples': [
+            'avoid',
+            'eye contact',
+            'ignore'
+          ]
+        }
+      ]
+    }
+  ]
+};
+
+module.exports = Questions;
