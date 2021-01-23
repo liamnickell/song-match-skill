01d8c4af83b75e7f67f113797185e597c6b5dbd1
Liam Nickell <liam.nickell25@gmail.com>
2021-01-23 01:24:52 -0600

    added answer intent handler to request handlers

--- lambda/index.js
+++ lambda/index.js
@@ -6,6 +6,7 @@ const ExitIntentHandler = require('./intents/exit.js');
 const HelpIntentHandler = require('./intents/help.js');
 const RepeatIntentHandler = require('./intents/repeat.js');
 const ArtistIntentHandler = require('./intents/artist.js');
+const AnswerIntentHandler = require('./intents/answer.js');
 const FallbackHandler = require('./intents/fallback.js');
 const IntentReflectorHandler = require('./intents/reflector.js');
 const SaveResponseInterceptor = require('./interceptors/save-response.js');
@@ -21,6 +22,7 @@ exports.handler = Alexa.SkillBuilders.custom()
     HelpIntentHandler,
     RepeatIntentHandler,
     ArtistIntentHandler,
+    AnswerIntentHandler,
     FallbackHandler,
     IntentReflectorHandler,
   )
